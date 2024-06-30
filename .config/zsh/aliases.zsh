a40e03ab55316a75449e81ad20da6280ab0e3bc2
minhman2501 <phminhman2501@gmail.com>
2024-06-30 10:15:52 +0700

    feat: add lazygit alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -4,6 +4,9 @@ alias cl=clear
 alias zshconfig='nvim ~/.config/zsh/.zshrc'
 alias kittyconfig='nvim ~/.config/kitty/kitty.conf'
 
+#Lazygit
+alias lazy='lazygit'
+
 #Update 
 alias update='sudo dnf update -y'
 alias upgrade='sudo dnf upgrade -y'
